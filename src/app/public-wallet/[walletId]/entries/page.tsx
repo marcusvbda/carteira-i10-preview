a9bf4f521ddcaa69622bebec02d4bee835de4a85
Vinicius Bassalobre <bassalobre.vinicius@gmail.com>
2024-06-18 18:52:54 -0300

    update 5

--- src/app/public-wallet/[walletId]/entries/page.tsx
+++ src/app/public-wallet/[walletId]/entries/page.tsx
@@ -1,6 +1,23 @@
 import { ReactNode } from 'react';
-import EntriesPage from '@/app/wallet/[walletId]/entries/page';
+import { Metadata } from 'next';
+import Datatables from '@/app/wallet/[walletId]/entries/_datatables';
+import { getEntriesData } from '@/app/wallet/[walletId]/entries/_methods';
+import { seo } from '@/constants/seo';
 
-export default function PublicEntriesPage(): ReactNode {
-	return <EntriesPage />;
+export const metadata: Metadata = seo.releases;
+interface IProps {
+	params: { walletId: string };
+}
+export default async function EntriesPage({
+	params,
+}: IProps): Promise<ReactNode> {
+	const { walletId } = params;
+
+	const data = await getEntriesData(walletId);
+
+	return (
+		<div className="page-container">
+			<Datatables data={data} />
+		</div>
+	);
 }
