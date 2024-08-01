e74471bfda3c593e97c8028f73f0b228883113c8
Vinicius Bassalobre <bassalobre.vinicius@gmail.com>
2024-07-31 21:59:41 -0300

    update vercel

--- src/components/cards/variation/index.tsx
+++ src/components/cards/variation/index.tsx
@@ -1,9 +1,10 @@
 import './_styles.scss';
 import { ReactNode } from 'react';
-import Link from 'next/link';
+import AspectRatio from '@/components/common/aspectRatio';
 import Icon from '@/components/common/icon';
 import SensitiveContent from '@/components/common/sensitiveContent';
 import Skeleton from '@/components/common/skeleton';
+import Tooltip from '@/components/common/tooltip';
 import Trend from '@/components/common/trend';
 import { useHelpers } from '@/hooks/helpers';
 import DefaultCard from '../default';
@@ -17,7 +18,29 @@ interface IProps {
 	variationAmount: number;
 }
 
-const Content = ({ profit, variation, variationAmount }: any): ReactNode => {
+const ContentProfit = ({ profit }: any): ReactNode => {
+	let profitTrendClass = '';
+	if (profit?.includes('-')) {
+		profitTrendClass = 'negative';
+	} else if (profit != '0') {
+		profitTrendClass = 'positive';
+	}
+	return (
+		<div className="card-variation--content">
+			<div className="card-variation--content-col">
+				<Trend
+					type={profitTrendClass}
+					size="20px"
+					value={profit}
+					transparent
+					strong
+				/>
+			</div>
+		</div>
+	);
+};
+
+const ContentVariation = ({ variation, variationAmount }: any): ReactNode => {
 	const helpers = useHelpers();
 	let varitionTrendClass = '';
 	if (variation?.includes('-')) {
@@ -26,12 +49,6 @@ const Content = ({ profit, variation, variationAmount }: any): ReactNode => {
 		varitionTrendClass = 'positive';
 	}
 
-	let profitTrendClass = '';
-	if (profit?.includes('-')) {
-		profitTrendClass = 'negative';
-	} else if (profit != '0') {
-		profitTrendClass = 'positive';
-	}
 	let formatedVariationAmount = '';
 	if (variationAmount < 0) {
 		formatedVariationAmount = `- ${helpers.formatMoney(variationAmount * -1)}`;
@@ -44,16 +61,7 @@ const Content = ({ profit, variation, variationAmount }: any): ReactNode => {
 	}
 	const formatedVariation = variation?.replace('-', '');
 	return (
-		<div className="card-variation--content">
-			<div className="card-variation--content-col">
-				<Trend
-					type={profitTrendClass}
-					size="20px"
-					value={profit}
-					transparent
-					strong
-				/>
-			</div>
+		<div className="card-variation--content" style={{ paddingLeft: 0 }}>
 			<div className="card-variation--content-col">
 				<Trend
 					type={varitionTrendClass}
@@ -84,20 +92,63 @@ export default function VariationCard({
 	if (loading) return <Skeleton height="122px" />;
 	return (
 		<DefaultCard className="card-variation" padding="12px">
-			<h4 style={{ display: 'flex' }}>
-				<div style={{ display: 'flex', flex: 1, alignItems: 'center', gap: 6 }}>
-					<Icon icon={`/images/theme/${icon}.svg`} width="14px" opacity="0.7" />
-					{title}
-				</div>
-				<Link style={{ flex: 1 }} href="#" className="variation">
+			<div>
+				<h4>
+					<div
+						style={{ display: 'flex', flex: 1, alignItems: 'center', gap: 6 }}
+					>
+						<div className="icon-ball-gray">
+							<Icon
+								icon={`/images/theme/${icon}.svg`}
+								width="14px"
+								opacity="0.7"
+							/>
+						</div>
+
+						{title}
+					</div>
+				</h4>
+				<ContentProfit profit={profit} />
+			</div>
+			<div>
+				<h4 style={{ display: 'flex', gap: 6, alignItems: 'center' }}>
 					Variação
-				</Link>
-			</h4>
-			<Content
-				variation={variation}
-				profit={profit}
-				variationAmount={variationAmount}
-			/>
+					<Tooltip
+						content={
+							<>
+								<strong>Variação: {variation}%</strong>
+								<br />
+								<br />
+								<strong>Rentabilidade: {profit}%</strong>
+								<br />
+								<br />
+								<strong>Variação</strong>: Informa de fato quanto foi o seu
+								ganho real, levando em conta o valor investido e o saldo atual.
+								Não considera o pagamento de dividendos.
+								<br />
+								<br />
+								<strong>Rentabilidade</strong>: Utilizamos a Rentabilidade
+								Ponderada, esse método permite ver a rentabilidade da carteira
+								excluindo distorções causadas pelos aportes ou retiradas.
+								Considera o pagamento de dividendos.
+								<br />
+								<br />
+								<strong>Importante</strong>: O sistema calcula a Rentabilidade
+								Ponderada apenas uma vez ao dia.
+							</>
+						}
+					>
+						<AspectRatio
+							src="/images/theme/ask-info.svg"
+							size={{ width: 10 }}
+						/>
+					</Tooltip>
+				</h4>
+				<ContentVariation
+					variation={variation}
+					variationAmount={variationAmount}
+				/>
+			</div>
 		</DefaultCard>
 	);
 }
--- src/components/common/tooltip/index.tsx
+++ src/components/common/tooltip/index.tsx
@@ -1,53 +1,35 @@
 import './_styles.scss';
-
-import { CSSProperties, ReactNode, useEffect, useRef, useState } from 'react';
+import { ReactNode } from 'react';
+import MuiTooltip from '@mui/material/Tooltip';
 interface IProps {
-	title?: string;
-	content: string;
+	title?: string | ReactNode;
+	content: string | ReactNode;
 	children: ReactNode;
-	position?: 'left' | 'right';
 }
 
 export default function Tooltip({
 	title,
 	content,
 	children,
-	position = 'left',
 }: IProps): ReactNode {
-	const [width, setWidth] = useState(200);
-	const [visible, setVisible] = useState(false);
-	const ref = useRef(null);
-	useEffect(() => {
-		setWidth((ref.current as any).offsetWidth);
-	}, [ref]);
+	const Content = (): ReactNode => {
+		return (
+			<>
+				{title && (
+					<>
+						<strong>{title}</strong>
+						<br />
+						<br />
+					</>
+				)}
+				{content}
+			</>
+		);
+	};
 
 	return (
-		<div
-			className={`tooltip ${visible && 'show'}`}
-			onMouseEnter={() => setVisible(true)}
-			onMouseLeave={() => setVisible(false)}
-		>
-			<svg
-				xmlns="http://www.w3.org/2000/svg"
-				width="13"
-				height="5"
-				viewBox="0 0 13 5"
-				fill="none"
-			>
-				<path
-					d="M5.1784 1.46845L3.00345 3.88506C2.36472 4.59475 1.45479 5 0.5 5H12.5C11.5452 5 10.6353 4.59475 9.99655 3.88506L7.8216 1.46845C7.11529 0.683658 5.88471 0.683659 5.1784 1.46845Z"
-					fill="#2C3A53"
-				/>
-			</svg>
-			<div
-				className={`tooltip-content ${position}`}
-				ref={ref}
-				style={{ '--tooltip-width': `${width}px` } as CSSProperties}
-			>
-				{title ? <strong>{title}</strong> : <> </>}
-				<div>{content}</div>
-			</div>
-			{children}
-		</div>
+		<MuiTooltip title={<Content />} placement="bottom" arrow>
+			<div>{children}</div>
+		</MuiTooltip>
 	);
 }
